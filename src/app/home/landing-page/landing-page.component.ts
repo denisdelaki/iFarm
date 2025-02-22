db3be670e4a1860dd94934c6a42d45e65bb2fbab
Denis kipyegon <denis.kipyegon@equitybank.co.ke>
2025-02-22 18:05:27 +0300

    rendered the home components on the landing page

--- src/app/home/landing-page/landing-page.component.ts
+++ src/app/home/landing-page/landing-page.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { AboutComponent } from '../about/about.component';
+import { ServicesComponent } from '../services/services.component';
+import { ContactsComponent } from '../contacts/contacts.component';
+import { TestimonialsComponent } from '../testimonials/testimonials.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactsComponent,
+    TestimonialsComponent
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
