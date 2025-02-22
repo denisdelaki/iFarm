fccd77e9f3870adeae6c5e636f1c05ebe95e97ae
Denis kipyegon <denis.kipyegon@equitybank.co.ke>
2025-02-22 17:52:41 +0300

    added the routes for the landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './home/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: LandingPageComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
