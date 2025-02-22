ef8f0b3f1004f8fcb62c4d8b37560800b0865b4a
Denis kipyegon <denis.kipyegon@equitybank.co.ke>
2025-02-22 20:18:07 +0300

    modified the landing page to accomodate the home section

--- src/app/home/landing-page/landing-page.component.ts
+++ src/app/home/landing-page/landing-page.component.ts
@@ -5,6 +5,7 @@ import { ServicesComponent } from '../services/services.component';
 import { ContactsComponent } from '../contacts/contacts.component';
 import { TestimonialsComponent } from '../testimonials/testimonials.component';
 import { MaterialModule } from '../../shared/material';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-landing-page',
@@ -14,6 +15,7 @@ import { MaterialModule } from '../../shared/material';
     ServicesComponent,
     ContactsComponent,
     TestimonialsComponent,
+    MatDividerModule
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
